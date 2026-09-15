6f801cbfe928db3e9f103ab25f39aef3be22f096
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-09-14 20:47:25 -0500

    Update saved.js

--- saved.js
+++ saved.js
@@ -15,37 +15,7 @@
 
 
 
-
 (function () {
-    var KEY = "disputeDetailAuthorized";
-
-    if (localStorage.getItem(KEY) !== "yes") {
-        var pwd = document.createElement("input");
-
-        pwd.type = "password";
-        pwd.placeholder = "Enter password";
-        pwd.style.cssText =
-            "position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);z-index:999999;padding:10px;font-size:16px;";
-
-        document.body.appendChild(pwd);
-        pwd.focus();
-
-        pwd.addEventListener("keydown", function (e) {
-            if (e.key === "Enter") {
-                if (pwd.value === "202608") {
-                    localStorage.setItem(KEY, "yes");
-                    document.body.removeChild(pwd);
-                    runScript();
-                } else {
-                    alert("Incorrect password");
-                    document.body.removeChild(pwd);
-                }
-            }
-        });
-
-        return;
-    }
-
     runScript();
 
     function runScript() {
